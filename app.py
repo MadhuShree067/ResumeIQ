985f062e9e3d9a976bc45188b9340f137939f6e3
Madhu Shree <madhushree4066@gmail.com>
2026-06-13 17:50:52 +0530

    Add professional UI and upload success page

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    file = request.files["resume"]
+
+    if file:
+
+        filepath = os.path.join(
+            app.config["UPLOAD_FOLDER"],
+            file.filename
+        )
+
+        file.save(filepath)
+
+        return render_template(
+            "success.html",
+            filename=file.filename
+        )
+
+    return "No file selected"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
